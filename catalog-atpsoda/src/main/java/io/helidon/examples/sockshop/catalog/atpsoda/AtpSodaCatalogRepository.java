6c6f4a56b2309af42a327e7474a9b7eef2c70d43
shashirsb <shasrama@SHASRAMA-IN.in.oracle.com>
2020-10-27 12:51:00 +0530

    adding

--- catalog-atpsoda/src/main/java/io/helidon/examples/sockshop/catalog/atpsoda/AtpSodaCatalogRepository.java
+++ catalog-atpsoda/src/main/java/io/helidon/examples/sockshop/catalog/atpsoda/AtpSodaCatalogRepository.java
@@ -7,14 +7,6 @@
 
 package io.helidon.examples.sockshop.catalog.atpsoda;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
@@ -24,19 +16,12 @@ import javax.inject.Inject;
 import io.helidon.examples.sockshop.catalog.CatalogRepository;
 import io.helidon.examples.sockshop.catalog.DefaultCatalogRepository;
 
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.model.Indexes;
-import com.mongodb.client.model.Sorts;
+
 import org.bson.BsonDocument;
 import org.bson.conversions.Bson;
 import org.eclipse.microprofile.opentracing.Traced;
 
 import io.helidon.examples.sockshop.catalog.atpsoda.AtpSodaProducers;
-
-import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Filters.or;
-
-///////////////////////
 
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -60,8 +45,13 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.HashMap;
 import java.util.stream.Stream;
-
-
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import oracle.soda.rdbms.OracleRDBMSClient;
 import oracle.soda.OracleDatabase;
@@ -80,7 +70,7 @@ import org.apache.commons.lang3.StringUtils;
 
 /**
  * An implementation of {@link io.helidon.examples.sockshop.catalog.CatalogRepository}
- * that that uses MongoDB as a backend data store.
+ * that that uses ATPSODA as a backend data store.
  */
 @ApplicationScoped
 @Specializes
@@ -120,13 +110,13 @@ public class AtpSodaCatalogRepository extends DefaultCatalogRepository {
             try {
 
                 // Find a documents in the collection.
-            String _tagFilter = tagsFilter(tags);
-            System.out.println("{\"tag\" : {\"$in\" : [formal, red, magic]}}");
-            System.out.println(_tagFilter);
-            OracleDocument filterSpec =
-            //this.db.createDocumentFromString(_tagFilter);
-            this.db.createDocumentFromString(_tagFilter);
-               c = col.find().filter(filterSpec).getCursor();
+                String _tagFilter = tagsFilter(tags);
+                System.out.println("{\"tag\" : {\"$in\" : [formal, red, magic]}}");
+                System.out.println(_tagFilter);
+                OracleDocument filterSpec =
+                    //this.db.createDocumentFromString(_tagFilter);
+                    this.db.createDocumentFromString(_tagFilter);
+                c = col.find().filter(filterSpec).getCursor();
 
                 OracleDocument resultDoc;
                 while (c.hasNext()) {
@@ -339,19 +329,19 @@ public class AtpSodaCatalogRepository extends DefaultCatalogRepository {
     }
 
 
-/**
+    /**
      * Helper method to create tags filter.
      *
      * @param tags a comma-separated list of tags; can be {@code null}
      *
-     * @return a MongoDB filter for the specified tags
+     * @return a ATPSODA DB filter for the specified tags
      */
     private String tagsFilter(String tags) {
         String _tagFilter = "";
         if (tags != null && !"".equals(tags)) {
-            List<String> _tags = Arrays.asList(tags.split("\\s*,\\s*"));
-            String step1 = StringUtils.join(_tags, "\", \"");// Join with ", "
-            String step2 = StringUtils.wrap(step1, "\"");// Wrap step1 with "
+            List < String > _tags = Arrays.asList(tags.split("\\s*,\\s*"));
+            String step1 = StringUtils.join(_tags, "\", \""); // Join with ", "
+            String step2 = StringUtils.wrap(step1, "\""); // Wrap step1 with "
             _tagFilter = "{\"tag\" : {\"$in\" : [" + step2 + "]}}";
             return _tagFilter;
         } else {
@@ -362,47 +352,47 @@ public class AtpSodaCatalogRepository extends DefaultCatalogRepository {
 
 
 
-public String createData() {
-    // Create a collection with the name "MyJSONCollection".
-    // This creates a database table, also named "MyJSONCollection", to store the collection.
-
-    String stringToParse = "[{\"count\":115,\"description\":\"For all those leg lovers out there. A perfect example of a swivel chair trained calf. Meticulously trained on a diet of sitting and Pina Coladas. Phwarr...\",\"id\":\"a0a4f044-b040-410d-8ead-4de0446aec7e\",\"imageUrl\":[\"/catalogue/images/bit_of_leg_1.jpeg\",\"/catalogue/images/bit_of_leg_2.jpeg\"],\"name\":\"Nerd leg\",\"price\":7.99,\"tag\":[\"blue\",\"skin\"]},{\"count\":801,\"description\":\"We were not paid to sell this sock. It's just a bit geeky.\",\"id\":\"d3588630-ad8e-49df-bbd7-3167f7efb246\",\"imageUrl\":[\"/catalogue/images/youtube_1.jpeg\",\"/catalogue/images/youtube_2.jpeg\"],\"name\":\"YouTube.sock\",\"price\":10.99,\"tag\":[\"formal\",\"geek\"]},{\"count\":127,\"description\":\"Keep it simple.\",\"id\":\"zzz4f044-b040-410d-8ead-4de0446aec7e\",\"imageUrl\":[\"/catalogue/images/classic.jpg\",\"/catalogue/images/classic2.jpg\"],\"name\":\"Classic\",\"price\":12,\"tag\":[\"green\",\"brown\"]},{\"count\":808,\"description\":\"enim officia aliqua excepteur esse deserunt quis aliquip nostrud anim\",\"id\":\"819e1fbf-8b7e-4f6d-811f-693534916a8b\",\"imageUrl\":[\"/catalogue/images/WAT.jpg\",\"/catalogue/images/WAT2.jpg\"],\"name\":\"Figueroa\",\"price\":14,\"tag\":[\"formal\",\"green\",\"blue\"]},{\"count\":820,\"description\":\"Ready for action. Engineers: be ready to smash that next bug! Be ready, with these super-action-sport-masterpieces. This particular engineer was chased away from the office with a stick.\",\"id\":\"510a0d7e-8e83-4193-b483-e27e09ddc34d\",\"imageUrl\":[\"/catalogue/images/puma_1.jpeg\",\"/catalogue/images/puma_2.jpeg\"],\"name\":\"SuperSport XL\",\"price\":15,\"tag\":[\"formal\",\"black\",\"sport\"]},{\"count\":175,\"description\":\"consequat amet cupidatat minim laborum tempor elit ex consequat in\",\"id\":\"837ab141-399e-4c1f-9abc-bace40296bac\",\"imageUrl\":[\"/catalogue/images/catsocks.jpg\",\"/catalogue/images/catsocks2.jpg\"],\"name\":\"Cat socks\",\"price\":15,\"tag\":[\"formal\",\"green\",\"brown\"]},{\"count\":738,\"description\":\"A mature sock, crossed, with an air of nonchalance.\",\"id\":\"808a2de1-1aaa-4c25-a9b9-6612e8f29a38\",\"imageUrl\":[\"/catalogue/images/cross_1.jpeg\",\"/catalogue/images/cross_2.jpeg\"],\"name\":\"Crossed\",\"price\":17.32,\"tag\":[\"red\",\"formal\",\"blue\",\"action\"]},{\"count\":438,\"description\":\"proident occaecat irure et excepteur labore minim nisi amet irure\",\"id\":\"3395a43e-2d88-40de-b95f-e00e1502085b\",\"imageUrl\":[\"/catalogue/images/colourful_socks.jpg\",\"/catalogue/images/colourful_socks.jpg\"],\"name\":\"Colourful\",\"price\":18,\"tag\":[\"blue\",\"brown\"]},{\"count\":1,\"description\":\"Socks fit for a Messiah. You too can experience walking in water with these special edition beauties. Each hole is lovingly proggled to leave smooth edges. The only sock approved by a higher power.\",\"id\":\"03fef6ac-1896-4ce8-bd69-b798f85c6e0b\",\"imageUrl\":[\"/catalogue/images/holy_1.jpeg\",\"/catalogue/images/holy_2.jpeg\"],\"name\":\"Holy\",\"price\":99.99,\"tag\":[\"magic\",\"action\"]}]";
-    try {
-        // pass the path to the file as a parameter 
-        // String stringToParse = "";
-        // stringToParse = new String(Files.readAllBytes(Paths.get("./../"+fileName)));
-
-        JSONParser parser = new JSONParser();
-        JSONObject jsonObjects = new JSONObject();
-        JSONArray jsonArray = (JSONArray) parser.parse(stringToParse.replace("\\", ""));
-
-
-        AtpSodaProducers asp = new AtpSodaProducers();
-        this.db = asp.dbConnect();
-
+    public String createData() {
         // Create a collection with the name "MyJSONCollection".
-        // This creates a database table, also named "MyJSONCollection", to store the collection.\
+        // This creates a database table, also named "MyJSONCollection", to store the collection.
 
-        OracleCollection col = this.db.admin().createCollection("socks");
+        String stringToParse = "[{\"count\":115,\"description\":\"For all those leg lovers out there. A perfect example of a swivel chair trained calf. Meticulously trained on a diet of sitting and Pina Coladas. Phwarr...\",\"id\":\"a0a4f044-b040-410d-8ead-4de0446aec7e\",\"imageUrl\":[\"/catalogue/images/bit_of_leg_1.jpeg\",\"/catalogue/images/bit_of_leg_2.jpeg\"],\"name\":\"Nerd leg\",\"price\":7.99,\"tag\":[\"blue\",\"skin\"]},{\"count\":801,\"description\":\"We were not paid to sell this sock. It's just a bit geeky.\",\"id\":\"d3588630-ad8e-49df-bbd7-3167f7efb246\",\"imageUrl\":[\"/catalogue/images/youtube_1.jpeg\",\"/catalogue/images/youtube_2.jpeg\"],\"name\":\"YouTube.sock\",\"price\":10.99,\"tag\":[\"formal\",\"geek\"]},{\"count\":127,\"description\":\"Keep it simple.\",\"id\":\"zzz4f044-b040-410d-8ead-4de0446aec7e\",\"imageUrl\":[\"/catalogue/images/classic.jpg\",\"/catalogue/images/classic2.jpg\"],\"name\":\"Classic\",\"price\":12,\"tag\":[\"green\",\"brown\"]},{\"count\":808,\"description\":\"enim officia aliqua excepteur esse deserunt quis aliquip nostrud anim\",\"id\":\"819e1fbf-8b7e-4f6d-811f-693534916a8b\",\"imageUrl\":[\"/catalogue/images/WAT.jpg\",\"/catalogue/images/WAT2.jpg\"],\"name\":\"Figueroa\",\"price\":14,\"tag\":[\"formal\",\"green\",\"blue\"]},{\"count\":820,\"description\":\"Ready for action. Engineers: be ready to smash that next bug! Be ready, with these super-action-sport-masterpieces. This particular engineer was chased away from the office with a stick.\",\"id\":\"510a0d7e-8e83-4193-b483-e27e09ddc34d\",\"imageUrl\":[\"/catalogue/images/puma_1.jpeg\",\"/catalogue/images/puma_2.jpeg\"],\"name\":\"SuperSport XL\",\"price\":15,\"tag\":[\"formal\",\"black\",\"sport\"]},{\"count\":175,\"description\":\"consequat amet cupidatat minim laborum tempor elit ex consequat in\",\"id\":\"837ab141-399e-4c1f-9abc-bace40296bac\",\"imageUrl\":[\"/catalogue/images/catsocks.jpg\",\"/catalogue/images/catsocks2.jpg\"],\"name\":\"Cat socks\",\"price\":15,\"tag\":[\"formal\",\"green\",\"brown\"]},{\"count\":738,\"description\":\"A mature sock, crossed, with an air of nonchalance.\",\"id\":\"808a2de1-1aaa-4c25-a9b9-6612e8f29a38\",\"imageUrl\":[\"/catalogue/images/cross_1.jpeg\",\"/catalogue/images/cross_2.jpeg\"],\"name\":\"Crossed\",\"price\":17.32,\"tag\":[\"red\",\"formal\",\"blue\",\"action\"]},{\"count\":438,\"description\":\"proident occaecat irure et excepteur labore minim nisi amet irure\",\"id\":\"3395a43e-2d88-40de-b95f-e00e1502085b\",\"imageUrl\":[\"/catalogue/images/colourful_socks.jpg\",\"/catalogue/images/colourful_socks.jpg\"],\"name\":\"Colourful\",\"price\":18,\"tag\":[\"blue\",\"brown\"]},{\"count\":1,\"description\":\"Socks fit for a Messiah. You too can experience walking in water with these special edition beauties. Each hole is lovingly proggled to leave smooth edges. The only sock approved by a higher power.\",\"id\":\"03fef6ac-1896-4ce8-bd69-b798f85c6e0b\",\"imageUrl\":[\"/catalogue/images/holy_1.jpeg\",\"/catalogue/images/holy_2.jpeg\"],\"name\":\"Holy\",\"price\":99.99,\"tag\":[\"magic\",\"action\"]}]";
+        try {
+            // pass the path to the file as a parameter 
+            // String stringToParse = "";
+            // stringToParse = new String(Files.readAllBytes(Paths.get("./../"+fileName)));
 
-        col.admin().truncate();
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObjects = new JSONObject();
+            JSONArray jsonArray = (JSONArray) parser.parse(stringToParse.replace("\\", ""));
 
-        for (int i = 0; i < jsonArray.size(); i++) {
 
-            // Create a JSON document.
-            OracleDocument doc =
-                this.db.createDocumentFromString(jsonArray.get(i).toString());
+            AtpSodaProducers asp = new AtpSodaProducers();
+            this.db = asp.dbConnect();
 
-            // Insert the document into a collection.
-            col.insert(doc);
+            // Create a collection with the name "MyJSONCollection".
+            // This creates a database table, also named "MyJSONCollection", to store the collection.\
 
+            OracleCollection col = this.db.admin().createCollection("socks");
+
+            col.admin().truncate();
+
+            for (int i = 0; i < jsonArray.size(); i++) {
+
+                // Create a JSON document.
+                OracleDocument doc =
+                    this.db.createDocumentFromString(jsonArray.get(i).toString());
+
+                // Insert the document into a collection.
+                col.insert(doc);
+
+            }
+
+        } catch (OracleException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-    } catch (OracleException e) {
-        e.printStackTrace();
-    } catch (Exception e) {
-        e.printStackTrace();
+        return "successfully created socks collection !!!";
     }
-    return "successfully created socks collection !!!";
-}
 }
